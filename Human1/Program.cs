ee9beb32d7077320c0d62af82af2c7de83fff315
1 <1@DESKTOP-LSHE6BO>
2023-03-05 15:13:04 +0300

    Добавьте файлы проекта.

---
+++ Human1/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    public class Human
+    {
+        public string surname;
+        public string birthday;
+
+        public class Teacher : Human
+        {
+            public string[] subject;
+            public string[] jobs;
+            public string working;
+            public string worked;
+            //конструктор, который создаёт обьект этого класса и добавляет его в список, такой есть в каждом классе.
+            static public List<Teacher> Constract(List<Teacher> teacher, string surname, string birthday, string[] jobs, string[] subject, string worked, string working)
+            {
+                Teacher person = new Teacher();
+
+                person.surname = surname;
+                person.birthday = birthday;
+                person.jobs = jobs;
+                person.subject = subject;
+                person.worked = worked;
+                person.working = working;
+
+                teacher.Add(person);
+                return teacher;
+            }
+        }
+
+        public class Tech : Human
+        {
+            public string[] jobs;
+            static public List<Tech> Constract(List<Tech> tech, string surname, string birthday, string[] jobs)
+            {
+                Tech person = new Tech();
+
+                person.surname = surname;
+                person.birthday = birthday;
+                person.jobs = jobs;
+
+                tech.Add(person);
+
+                return tech;
+            }
+        }
+
+        public class Boss : Human
+        {
+            //словарь.
+            public Dictionary<string, List<string>> order;
+            static public List<Boss> Constract(List<Boss> boss, string surname, string birthday)
+            {
+                Boss person = new Boss();
+
+                person.surname = surname;
+                person.birthday = birthday;
+                person.order = new Dictionary<string, List<string>>();
+                boss.Add(person);
+
+                return boss;
+            }
+
+            //он нужен для разделения приказов на 4 группы, у каждой свои приказы.
+
+            //Я делил на 3 группы, потому что боссы сами себе не приказывают
+            static public Dictionary<string, List<string>> Decanat(Dictionary<string, List<string>> order)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine("Кому назначаются приказы?");
+                    string direction = Console.ReadLine();
+                    Console.WriteLine("Какие приказы назначаются?");
+                    List<string> orders = Console.ReadLine().Split(' ').ToList();
+                    order[direction] = orders;
+
+                    Console.WriteLine("Ввести для другого направления?");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "Нет" || answer == "нет")
+                    {
+                        break;
+                    }
+                }
+                return order;
+            }
+        }
+
+        public class Student : Human
+        {
+            //а тут словарь для удобного разделения на предмет-долг и у какого учителя(по большей части просто выёбываюсь), в предмет будут суваться строка - предмет, оценка , препод
+            //если долг, то будет неуд
+
+            public Dictionary<string, string[]> letters; //предмет - оценка, препод   //так удобно делать выборки по долгам, проверять, если ключ == 2, то выводить чо просят
+            static public List<Student> Constract(List<Student> student, string surname, string birthday)
+            {
+                Student person = new Student();
+
+                person.surname = surname;
+                person.birthday = birthday;
+                person.letters = new Dictionary<string, string[]>();
+
+                student.Add(person);
+
+                return student;
+            }
+
+            static public Dictionary<string, string[]> Input(Dictionary<string, string[]> letters)
+            {
+                Console.WriteLine("Сколько предметов у студента?");
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Введите предмет и преподавателя через пробел");
+                    string inf = Console.ReadLine();
+                    string[] information = inf.Split(' ');
+                    Console.WriteLine("Введите оценку по этому предмету");
+                    string letter = Console.ReadLine();
+
+                    letters[letter] = information;  //information -оценка, препод
+                }
+
+                return letters;
+            }
+        }
+        public static void Main()
+        {
+
+        }
+    }
+}
